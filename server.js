c383668c48a256b4290afd0c61d0954a5739c214
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-07-03 12:26:36 -0400

    create server.js

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+//require modules
 const express = require("express");
-const fs = require("fs");
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes.js')
+const htmlRoutes = require('./routes/htmlRoutes.js');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+//middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
+//use routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-
-
-
-
+//listen for port
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
