2b6ad68f56f8df3925957cc8fb3f3f043164938e
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-07-03 19:07:24 -0400

    try update function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,6 +4,7 @@ let notes = require("../../db/db.json");
 const path = require("path");
 const fs = require("fs");
 
+
 //get all notes
 router.get("/notes", (req, res) => {
     let results = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));;
@@ -11,6 +12,7 @@ router.get("/notes", (req, res) => {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+    update();
 });
 
 //get single note by ID
@@ -20,19 +22,20 @@ router.get("/notes/:id", (req, res) => {
       res.json(result);
     } else {
       res.send(404);
-    }
+    } update();
 });
 
 //post a note with new ID
 router.post("/notes", (req, res) => {
-req.body.id = notes.length.toString();
+    req.body.id = notes.length.toString();
 
-if (!validateNote(req.body)) {
-    res.status(400).send("The note is not properly formatted.");
-} else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-}
+    if (!validateNote(req.body)) {
+        res.status(400).send("The note is not formatted properly.");
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+    update();
 });
 
 //delete note
@@ -40,6 +43,12 @@ router.delete('/notes/:id', (req, res) => {
     res.send("Note deleted")
 })
 
-
+//updates the json file whenever a note is added or deleted
+function update() {
+    fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+        if (err) throw err;
+        return true;
+    });
+}
 
 module.exports = router;
