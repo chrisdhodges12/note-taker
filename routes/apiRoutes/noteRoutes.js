32c8f539fa2bbdf1ce2b89c4258362e12b19a298
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-07-03 20:04:58 -0400

    delete function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,43 +1,56 @@
 const router = require("express").Router();
-const { filterByQuery, findById, createNewNote, validateNote, } = require("../../lib/notes.js");
+const {
+  filterByQuery,
+  findById,
+  createNewNote,
+  validateNote,
+} = require("../../lib/notes.js");
 let notes = require("../../db/db.json");
 const path = require("path");
 const fs = require("fs");
 
-
 //get all notes
 router.get("/notes", (req, res) => {
-    let results = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
+  let results = JSON.parse(fs.readFileSync("./db/db.json",));
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
 });
 
 //get single note by ID
 router.get("/notes/:id", (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 //post a note with new ID
 router.post("/notes", (req, res) => {
-    req.body.id = notes.length.toString();
+  req.body.id = notes.length.toString();
 
-    if (!validateNote(req.body)) {
-        res.status(400).send("The note is not formatted properly.");
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    }
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not formatted properly.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 //delete note
+router.delete("/notes/:id", async (req, res) => {
+  const deleteThis = findById(req.params.id, notes);
+  if (deleteThis) {
+    let current = JSON.parse(fs.readFileSync("./db/db.json"));
+    const update = await current.filter((note) => note.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(update));
+    const newNotes = JSON.parse(fs.readFileSync("./db/db.json"));
 
-
+    return res.json(newNotes);
+  }
+});
 
 module.exports = router;
